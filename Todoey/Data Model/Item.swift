490d61dec052fcb09364ff9be04cfe68b05e3979
Steve Rustom <ejr00@mail.aub.edu>
2019-01-22 16:57:15 +0200

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Steve Rustom on 1/22/19.
+//  Copyright © 2019 Steve Rustom. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
